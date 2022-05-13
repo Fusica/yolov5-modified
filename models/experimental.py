70cc041b47995080506c1a86dba98c63f4e08837
max <1733169585a@gmail.com>
2022-05-13 11:12:24 +0800

    add poolformer

--- models/experimental.py
+++ models/experimental.py
@@ -12,6 +12,10 @@ from torch.nn import functional as F
 from models.common import Conv
 from utils.downloads import attempt_download
 
+from timm.models.layers import DropPath, trunc_normal_
+
+from common import C3
+
 
 class CrossConv(nn.Module):
     # Cross Convolution Downsample
@@ -368,5 +372,127 @@ class NONLocalBlock2D(_NonLocalBlockND):
                                               dimension=2, sub_sample=sub_sample,
                                               bn_layer=bn_layer, )
 
-# TODO add poolformer block into model
-# class Poolformer():
+
+class GroupNorm(nn.GroupNorm):
+    """
+    Group Normalization with 1 group.
+    Input: tensor in shape [B, C, H, W]
+    """
+
+    def __init__(self, num_channels, **kwargs):
+        super().__init__(1, num_channels, **kwargs)
+
+
+class Pooling(nn.Module):
+    """
+    Implementation of pooling for PoolFormer
+    --pool_size: pooling size
+    """
+
+    def __init__(self, pool_size=3):
+        super().__init__()
+        self.pool = nn.AvgPool2d(
+            pool_size, stride=1, padding=pool_size // 2, count_include_pad=False)
+
+    def forward(self, x):
+        return self.pool(x) - x
+
+
+class Mlp(nn.Module):
+    """
+    Implementation of MLP with 1*1 convolutions.
+    Input: tensor with shape [B, C, H, W]
+    """
+
+    def __init__(self, in_features, hidden_features=None,
+                 out_features=None, act_layer=nn.GELU, drop=0.):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.fc1 = nn.Conv2d(in_features, hidden_features, 1)
+        self.act = act_layer()
+        self.fc2 = nn.Conv2d(hidden_features, out_features, 1)
+        self.drop = nn.Dropout(drop)
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Conv2d):
+            trunc_normal_(m.weight, std=.02)
+            if m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.drop(x)
+        return x
+
+
+class PoolformerBlock(nn.Module):
+    """
+    Implementation of one PoolFormer block.
+    --dim: embedding dim
+    --pool_size: pooling size
+    --mlp_ratio: mlp expansion ratio
+    --act_layer: activation
+    --norm_layer: normalization
+    --drop: dropout rate
+    --drop path: Stochastic Depth,
+        refer to https://arxiv.org/abs/1603.09382
+    --use_layer_scale, --layer_scale_init_value: LayerScale,
+        refer to https://arxiv.org/abs/2103.17239
+    """
+
+    def __init__(self, c1, dim, pool_size=3, mlp_ratio=4.,
+                 act_layer=nn.GELU, norm_layer=GroupNorm,
+                 drop=0., drop_path=0.,
+                 use_layer_scale=True, layer_scale_init_value=1e-5):
+
+        super().__init__()
+        self.conv = nn.Conv2d(c1, dim, 1)
+        self.norm1 = norm_layer(dim)
+        self.token_mixer = Pooling(pool_size=pool_size)
+        self.norm2 = norm_layer(dim)
+        mlp_hidden_dim = int(dim * mlp_ratio)
+        self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim,
+                       act_layer=act_layer, drop=drop)
+
+        # The following two techniques are useful to train deep PoolFormers.
+        self.drop_path = DropPath(drop_path) if drop_path > 0. \
+            else nn.Identity()
+        self.use_layer_scale = use_layer_scale
+        if use_layer_scale:
+            self.layer_scale_1 = nn.Parameter(
+                layer_scale_init_value * torch.ones((dim)), requires_grad=True)
+            self.layer_scale_2 = nn.Parameter(
+                layer_scale_init_value * torch.ones((dim)), requires_grad=True)
+
+    def forward(self, x):
+        x = self.conv(x)
+        if self.use_layer_scale:
+            x = x + self.drop_path(
+                self.layer_scale_1.unsqueeze(-1).unsqueeze(-1)
+                * self.token_mixer(self.norm1(x)))
+            x = x + self.drop_path(
+                self.layer_scale_2.unsqueeze(-1).unsqueeze(-1)
+                * self.mlp(self.norm2(x)))
+        else:
+            x = x + self.drop_path(self.token_mixer(self.norm1(x)))
+            x = x + self.drop_path(self.mlp(self.norm2(x)))
+        return x
+
+
+class C3PF(C3):
+    # C3 module with PoolformerBlock()
+    def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=1.0):
+        super().__init__(c1, c2, n, shortcut, g, e)
+        self.pf = nn.Sequential(*(PoolformerBlock(c2, c2) for _ in range(n)))
+        self.m = self.pf
+
+
+c3pf = C3PF(64, 64, 3)
+input = torch.randn(3, 64, 40, 40)
+output = c3pf(input)
+print(output.shape)
--- train.py
+++ train.py
@@ -474,7 +474,7 @@ def train(hyp, opt, device, callbacks):  # hyp is path/to/hyp.yaml or hyp dictio
 def parse_opt(known=False):
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', type=str, default=ROOT / 'yolov5n.pt', help='initial weights path')
-    parser.add_argument('--cfg', type=str, default='models/yolov5_J_AC_v8.yaml', help='model.yaml path')
+    parser.add_argument('--cfg', type=str, default='models/yolov5n_J_AC_v8.yaml', help='model.yaml path')
     parser.add_argument('--data', type=str, default=ROOT / 'data/coco128.yaml', help='dataset.yaml path')
     parser.add_argument('--hyp', type=str, default=ROOT / 'data/hyps/hyp.scratch-low.yaml', help='hyperparameters path')
     parser.add_argument('--epochs', type=int, default=300)
@@ -568,35 +568,36 @@ def main(opt, callbacks=Callbacks()):
     else:
         # Hyperparameter evolution metadata (mutation scale 0-1, lower_limit, upper_limit)
         meta = {
-            'lr0': (1, 1e-5, 1e-1),  # initial learning rate (SGD=1E-2, Adam=1E-3)
-            'lrf': (1, 0.01, 1.0),  # final OneCycleLR learning rate (lr0 * lrf)
-            'momentum': (0.3, 0.6, 0.98),  # SGD momentum/Adam beta1
-            'weight_decay': (1, 0.0, 0.001),  # optimizer weight decay
-            'warmup_epochs': (1, 0.0, 5.0),  # warmup epochs (fractions ok)
-            'warmup_momentum': (1, 0.0, 0.95),  # warmup initial momentum
-            'warmup_bias_lr': (1, 0.0, 0.2),  # warmup initial bias lr
-            'box': (1, 0.02, 0.2),  # box loss gain
-            'cls': (1, 0.2, 4.0),  # cls loss gain
-            'cls_pw': (1, 0.5, 2.0),  # cls BCELoss positive_weight
-            'obj': (1, 0.2, 4.0),  # obj loss gain (scale with pixels)
-            'obj_pw': (1, 0.5, 2.0),  # obj BCELoss positive_weight
+            'lr0': (0, 1e-5, 1e-1),  # initial learning rate (SGD=1E-2, Adam=1E-3)
+            'lrf': (0, 0.01, 1.0),  # final OneCycleLR learning rate (lr0 * lrf)
+            'momentum': (0, 0.6, 0.98),  # SGD momentum/Adam beta1
+            'weight_decay': (0, 0.0, 0.001),  # optimizer weight decay
+            'warmup_epochs': (0, 0.0, 5.0),  # warmup epochs (fractions ok)
+            'warmup_momentum': (0, 0.0, 0.95),  # warmup initial momentum
+            'warmup_bias_lr': (0, 0.0, 0.2),  # warmup initial bias lr
+            'box': (0, 0.02, 0.2),  # box loss gain
+            'cls': (0, 0.2, 4.0),  # cls loss gain
+            'cls_pw': (0, 0.5, 2.0),  # cls BCELoss positive_weight
+            'obj': (0, 0.2, 4.0),  # obj loss gain (scale with pixels)
+            'obj_pw': (0, 0.5, 2.0),  # obj BCELoss positive_weight
             'iou_t': (0, 0.1, 0.7),  # IoU training threshold
-            'anchor_t': (1, 2.0, 8.0),  # anchor-multiple threshold
-            'anchors': (2, 2.0, 10.0),  # anchors per output grid (0 to ignore)
+            'anchor_t': (0, 2.0, 8.0),  # anchor-multiple threshold
+            'anchors': (0, 0, 0),  # anchors per output grid (0 to ignore)
             'fl_gamma': (0, 0.0, 2.0),  # focal loss gamma (efficientDet default gamma=1.5)
-            'hsv_h': (1, 0.0, 0.1),  # image HSV-Hue augmentation (fraction)
-            'hsv_s': (1, 0.0, 0.9),  # image HSV-Saturation augmentation (fraction)
-            'hsv_v': (1, 0.0, 0.9),  # image HSV-Value augmentation (fraction)
-            'degrees': (1, 0.0, 45.0),  # image rotation (+/- deg)
-            'translate': (1, 0.0, 0.9),  # image translation (+/- fraction)
-            'scale': (1, 0.0, 0.9),  # image scale (+/- gain)
-            'shear': (1, 0.0, 10.0),  # image shear (+/- deg)
+            'pl_epsilon': (0.5, 1.0, 2.0),
+            'hsv_h': (0, 0.0, 0.1),  # image HSV-Hue augmentation (fraction)
+            'hsv_s': (0, 0.0, 0.9),  # image HSV-Saturation augmentation (fraction)
+            'hsv_v': (0, 0.0, 0.9),  # image HSV-Value augmentation (fraction)
+            'degrees': (0, 0.0, 45.0),  # image rotation (+/- deg)
+            'translate': (0, 0.0, 0.9),  # image translation (+/- fraction)
+            'scale': (0, 0.0, 0.9),  # image scale (+/- gain)
+            'shear': (0, 0.0, 10.0),  # image shear (+/- deg)
             'perspective': (0, 0.0, 0.001),  # image perspective (+/- fraction), range 0-0.001
-            'flipud': (1, 0.0, 1.0),  # image flip up-down (probability)
+            'flipud': (0, 0.0, 1.0),  # image flip up-down (probability)
             'fliplr': (0, 0.0, 1.0),  # image flip left-right (probability)
-            'mosaic': (1, 0.0, 1.0),  # image mixup (probability)
-            'mixup': (1, 0.0, 1.0),  # image mixup (probability)
-            'copy_paste': (1, 0.0, 1.0)}  # segment copy-paste (probability)
+            'mosaic': (0, 0.0, 1.0),  # image mixup (probability)
+            'mixup': (0, 0.0, 1.0),  # image mixup (probability)
+            'copy_paste': (0, 0.0, 1.0)}  # segment copy-paste (probability)
 
         with open(opt.hyp, errors='ignore') as f:
             hyp = yaml.safe_load(f)  # load hyps dict
